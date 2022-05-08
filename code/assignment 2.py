ef973139a0496905b0e115f63188f32c04e1d547
pettugadipranav <102520050+pettugadipranav@users.noreply.github.com>
2022-05-08 15:41:15 +0530

    Update assignment 2.py

--- code/assignment 2.py
+++ code/assignment 2.py
@@ -1,1 +1,11 @@
+import math
+from fractions import Fraction as frac
+
+x = 1
+a = frac(1, 2) * math.acos(frac((1 - x), (1 + x)))
+b = math.atan(math.sqrt(x))
+if (a == b):
+    print("yes")
+else:
+    print("no")
 
